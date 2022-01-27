6b76ec484f5002bd8287e78527a843a5af8adbc7
imanmansour86 <imanmansour86@gmail.com>
2022-01-26 21:46:51 -0800

    writing employee unit tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,35 +1,41 @@
 const Employee = require("../lib/employee.js");
 
 describe("Employee", () => {
-  it("Should create an object with name,id and email", () => {
-    const employee = new Employee("mike", 202, "mike@gmail.com");
+  describe("Initialization", () => {
+    it("Should create an object with name,id and email", () => {
+      const employee = new Employee("mike", 202, "mike@gmail.com");
 
-    expect(employee.name).toEqual("mike");
-    expect(employee.id).toEqual(202);
-    expect(employee.email).toEqual("ike@gmail.com");
+      expect(employee.name).toEqual("mike");
+      expect(employee.id).toEqual(202);
+      expect(employee.email).toEqual("mike@gmail.com");
+    });
+  });
+
+  describe("getName", () => {
+    it("Should get the name of the employee", () => {
+      const employee = new Employee("mike", 202, "mike@gmail.com");
+      expect(employee.getName()).toEqual("mike");
+    });
+  });
+
+  describe("getId", () => {
+    it("Should get the id of the employee", () => {
+      const employee = new Employee("mike", 202, "mike@gmail.com");
+      expect(employee.getId()).toEqual(202);
+    });
+  });
+
+  describe("getEmail", () => {
+    it("Should get the email of the employee", () => {
+      const employee = new Employee("mike", 202, "mike@gmail.com");
+      expect(employee.getEmail()).toEqual("mike@gmail.com");
+    });
+  });
+
+  describe("getRole", () => {
+    it("Should get the role of the employee", () => {
+      const employee = new Employee("mike", 202, "mike@gmail.com");
+      expect(employee.getRole()).toEqual("Employee");
+    });
   });
 });
-
-// class Employee {
-//     constructor(name, id, email) {
-//       this.name = name;
-//       this.id = id;
-//       this.email = email;
-//     }
-
-//     getName() {
-//       return this.name;
-//     }
-
-//     getId() {
-//       return this.id;
-//     }
-
-//     getEmail() {
-//       return this.email;
-//     }
-
-//     getRole() {
-//       return "Employee";
-//     }
-//   }
